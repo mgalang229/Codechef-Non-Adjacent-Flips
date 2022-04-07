f423fe6cdad9c07ac66601a4c565ad07b963276d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-04-07 10:56:59 +0800

    Update main.java

--- main.java
+++ main.java
@@ -16,20 +16,31 @@ public class Main {
 	
 	static class Solver { 
 		Solver() {
+			
+			// input
 			int n = in.nextInt();
 			String s = in.next();
+			
+			// best case
 			String z = "";
 			for(int i = 0; i < n; i++) {
 				z += "0";
 			}
+
+			// no need to do any operations
 			if(s.equals(z)) {
 				System.out.println(0);
 				return;
 			}
+
+			// if there is at least one group of 1s that are adjacent to each other,
+			// then we the minimum number of operations will be 2, otherwise 1
 			boolean adjacent = false;
 			for(int i = 0; i + 1 < n; i++) {
 				adjacent |= (s.charAt(i) == s.charAt(i + 1) && s.charAt(i) == '1');
 			}
+
+			// output
 			System.out.println(!adjacent ? 1 : 2);
 		}
 	}
